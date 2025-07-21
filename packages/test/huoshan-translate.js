8474404abdde4c9c6e59bfd4b7c681a25df29ca4
uikoo9 <uikoo9@qq.com>
2025-07-21 20:47:34 +0800

    feat(test): split md and translate ok

--- packages/test/huoshan-translate.js
+++ packages/test/huoshan-translate.js
@@ -1,25 +1,23 @@
 // qiao
 const { readFile } = require('qiao-file');
+
+// md
+const { splitMD } = require('md-segmenter');
+
 // llm
 const LLM = require('qiao-llm')({
   apiKey: require('./config.json').huoshan_key,
   baseURL: 'https://ark.cn-beijing.volces.com/api/v3',
 });
 
-// main
-async function main() {
-  // translate prompt
-  const translatePrompt = await readFile('./prompt-translate.md');
-
-  // translate md
-  const translateMD = await readFile('./translate.md');
-
+// translate
+async function translate(translatePrompt, mdContent) {
   // chat options
   const chatOptions = {
     model: 'ep-20250721164252-zzmtx',
     messages: [
       { role: 'system', content: translatePrompt },
-      { role: 'user', content: `请翻译下面的内容:${translateMD}` },
+      { role: 'user', content: `请翻译下面的内容:${mdContent}` },
     ],
     thinking: {
       type: 'disabled',
@@ -27,8 +25,24 @@ async function main() {
   };
 
   // go
-  const res = await LLM.chat(chatOptions);
-  console.log(res);
+  return await LLM.chat(chatOptions);
 }
 
-main();
+(async () => {
+  // translate prompt
+  const translatePrompt = await readFile('./prompt-translate.md');
+
+  // translate md
+  const translateMD = await readFile('./translate.md');
+  const mds = splitMD(translateMD);
+
+  // go
+  for (let i = 0; i < mds.length; i++) {
+    const mdContent = mds[i];
+    const translateRes = await translate(translatePrompt, mdContent);
+    console.log(`====${i}====`);
+    console.log(mdContent);
+    console.log('====after====');
+    console.log(translateRes);
+  }
+})();
