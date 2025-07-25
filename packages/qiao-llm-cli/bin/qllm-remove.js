0894774a4b0415231bdaf82282be5d9e6308633a
uikoo9 <uikoo9@qq.com>
2025-07-25 17:26:04 +0800

    feat(qiao-llm-cli): remove ok

---
+++ packages/qiao-llm-cli/bin/qllm-remove.js
@@ -0,0 +1,40 @@
+// qiao
+const cli = require('qiao-cli');
+
+// db
+const DB = require('qiao-config');
+const db = DB();
+
+/**
+ * remove
+ */
+const remove = async () => {
+  try {
+    // q a
+    const questions = [
+      {
+        type: 'input',
+        name: 'modelName',
+        message: '请输入要删除的模型名称：',
+      },
+    ];
+    const answers = await cli.ask(questions);
+
+    // del
+    const dbKey = answers.modelName;
+    await db.config(dbKey, null);
+    console.log('模型已删除。');
+
+    // list
+    const all = await db.all();
+    console.log(all);
+  } catch (e) {
+    console.log('删除模型出错。');
+    console.log();
+
+    console.log(e);
+  }
+};
+
+// cmd for file
+cli.cmd.command('remove').description('删除一个模型').action(remove);
--- packages/qiao-llm-cli/bin/qllm.js
+++ packages/qiao-llm-cli/bin/qllm.js
@@ -6,6 +6,7 @@ const cli = require('qiao-cli');
 // cmds
 require('./qllm-add.js');
 require('./qllm-list.js');
+require('./qllm-remove.js');
 require('./qllm-version.js');
 
 // parse
